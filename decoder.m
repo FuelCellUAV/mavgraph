c4f6529783050994f12a3c16f2d67d0cef983fbe
Simon Howroyd <howroydlsu@gmail.com>
2013-06-12 12:14:56 +0100

    Working
    Saves to a .mat file

--- decoder.m
+++ decoder.m
@@ -7,7 +7,7 @@ clear all
 fid = fopen(strcat(PathName,FileName));
 file = textscan(fid,'%s','delimiter','\n');
 fclose(fid);
-clear fid PathName FileName FilterIndex ans
+%clear fid PathName FileName FilterIndex ans
 
 %% Decode & packetise
 data = struct;
@@ -29,73 +29,38 @@ for x=1:size(file{1,1},1)
     end
     % all packets IDs are now listed as a variable in the "data" structure
 end
-clearvars -except data
+%clearvars -except data
 
 %% Decode packets
 packetlist = fieldnames(data);
 for x=1:size(packetlist,1) % find variable in structure
-    for y=1:size(data.(packetlist{x}),1) % find next row in variable
-        thisline = data.(packetlist{x})(1,:);
+    thispacket = data.(packetlist{x});
+    for y=1:size(thispacket,1) % find next row in variable
+        thisline = thispacket(y,:);
         
         % time of row in serial format
-        time = datenum(strrep(data.mavlink_vfr_hud_t(x,1),'T',' '));
-        name = thisline{8};
+        try
+            time = datenum(strrep(thisline(1,1),'T',' '));
+            name = thisline{8};
+        catch
+            continue
+        end
         
         for z=9:2:size(thisline,2)
-            out.(thisline{8})(thisline{9}){y,1} = str2double(thisline(10));
+            try
+                out.(thisline{8}).(thisline{z}){y,1} = str2double(thisline(z+1));
+            catch
+                continue;
+            end
         end
     end
 end
+%clearvars -except out
 
-%% Parse mavlink_vfr_hud_t
-
-% Airspeed
-try
-    for x=1:size(data.mavlink_vfr_hud_t,1)
-        vfr_hud.airspeed(x,1) = str2double(data.mavlink_vfr_hud_t(x,10));
-    end
-catch exception
-    disp "No airspeed data"
-end
-
-% Groundspeed
-try
-    for x=1:size(data.mavlink_vfr_hud_t,1)
-        vfr_hud.groundspeed(x,1) = str2double(data.mavlink_vfr_hud_t(x,12));
-    end
-catch exception
-   disp "No groundspeed data"
-end
-
-% Altitude
-try
-    for x=1:size(data.mavlink_vfr_hud_t,1)
-        vfr_hud.altitude(x,1) = str2double(data.mavlink_vfr_hud_t(x,14));
-    end
-catch exception
-    disp "No altitude data"
-end
-
-% Throttle
-try
-    for x=1:size(data.mavlink_vfr_hud_t,1)
-        vfr_hud.throttle(x,1) = str2double(data.mavlink_vfr_hud_t(x,20));
-    end
-catch exception
-    disp "No throttle data"
-end
-
-% Time
-try
-    for x=1:size(data.mavlink_vfr_hud_t,1)
-        time = strrep(data.mavlink_vfr_hud_t(x,1),'T',' ');
-        vfr_hud.time(x,1) = datenum(time);
-    end
-catch exception
-    disp "No time data"
-end
-
-clear exception x
+%% Save and end
+save(strrep(FileName,'csv',''),'-struct','out');
+disp(strcat('Saved to ./',FileName,'.mat'));
+clear all
 
 %% End
 disp "Done!"
